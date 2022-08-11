7b22e3b32faec4713929a56c66fbd38949775b0a
bedo <76664175+bedirhansarihan@users.noreply.github.com>
2022-08-11 03:32:50 +0300

    upd

--- 2 - abstract_factory_pattern/main.py
+++ 2 - abstract_factory_pattern/main.py
@@ -1,3 +1,4 @@
+import typing
 from abc import abstractmethod, ABC
 
 
@@ -86,3 +87,63 @@ class Ferrari(F1TeamFactory):
 
     def get_second_driver(self) -> SecondDriver:
         return Sainz()
+
+
+def select_team() -> F1TeamFactory:
+    teams = {
+        "Mercedes": Mercedes(),
+        "Ferrari": Ferrari()
+    }
+
+    while True:
+
+        choose_team = input(f"choose team ({', '.join(teams)}): ")
+
+        try:
+            return teams[choose_team]
+            break
+        except KeyError:
+            print("Key Error")
+
+if __name__ == '__main__':
+
+
+    f1_team = select_team()
+
+    driver_1: FirstDriver = f1_team.get_first_driver()
+    driver_2: SecondDriver = f1_team.get_second_driver()
+
+    driver_1_name = driver_1.get_full_name()
+    driver_1_rank = driver_1.get_rank()
+
+
+
+    print(driver_1)
+    print(driver_2)
+    print(driver_1_name)
+    print(driver_1_rank)
+
+
+
+"""
+output:
+    choose team (Mercedes, Ferrari): Mercedes
+    <__main__.Hamilton object at 0x000001FC33B14D30>
+    <__main__.Russel object at 0x000001FC33B14DA0>
+    Lewis Hamilton
+    94
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
